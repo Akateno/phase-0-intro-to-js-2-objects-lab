309c4b2a51fbc51f7ddd5c869e6acaf49da88edb
Erick Vargas <erickkvargas@gmail.com>
2022-04-05 15:03:25 -0700

    commit tree cb48d694a9fe6f1c4fbfe04b71f67d6ede1e6744

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name : "Erick",
+    streetAddress : "Pike"
+
+};
+function updateEmployeeWithKeyAndValue(employee, name, cameron) {
+    const newEmployee = { ...employee };
+  
+    newEmployee["name"] = "Sam";
+    newEmployee["streetAddress"]="11 Broadway"
+  
+    return newEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    
+  
+    return employee
+  }
+  function deleteFromEmployeeByKey(obj, key, value) {
+    const newEmployee = { ...employee };
+  
+    delete newEmployee.name;
+  
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    delete employee.name;
+    
+  
+    return employee
+  }
